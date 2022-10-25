647b4b1b51c9d3f663e750d5f9baccdf0ad3f5c2
siemensstadt29 <stefan_gierke@web.de>
2022-10-25 19:42:39 +0200

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -28,5 +28,5 @@ export async function run() {
   const mailGUID = encodeURIComponent(item.itemId);
   console.log("Halli hallooo");
 
-  $("#powerapps-iframe").attr("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID);
+  document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID);
 }
