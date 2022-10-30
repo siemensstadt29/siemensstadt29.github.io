23f3b5085200f28dc7b27cea825a769447cd2c89
siemensstadt29 <stefan_gierke@web.de>
2022-10-30 22:18:29 +0100

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -28,9 +28,11 @@ async function run() {
   //const from = item.from.emailAddress;
   //const toRecipient = item.to[0]["emailAddress"];
   const mailGUID = encodeURIComponent(item.itemId);
-  const subject = item.subject;
+  const subject = encodeURIComponent(item.subject);
+  const date = encodeURIComponent(item.dateTimeCreated);
 
   console.log("Halli hallooo");
+  console.log("Datum = " + date);
 
-  document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject);
+  //document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject + "&mailDate=" + date);
 }
