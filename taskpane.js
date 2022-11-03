f06eb16e23fa9afd973a8c1dc63da72f66f33857
siemensstadt29 <stefan_gierke@web.de>
2022-11-03 20:13:26 +0100

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -34,7 +34,6 @@ async function run() {
 
   console.log("Halli hallooo");
   console.log("Datum = " + date);
-  console.log("Visibility = " + Office.VisibilityMode);
 
   document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject + "&mailDate=" + date);
 }
