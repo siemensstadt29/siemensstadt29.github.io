114951deb78b4e116937ce30142fc45030333555
siemensstadt29 <stefan_gierke@web.de>
2022-11-18 22:59:04 +0100

    Revoke changes to original files

--- taskpane.js
+++ taskpane.js
@@ -10,19 +10,6 @@ Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     console.log("Das Skript wurde erfolgreich geladen!");
 
-    //Register an event handler to identify when messages are selected.
-    Office.context.mailbox.addHandlerAsync(Office.EventType.SelectedItemsChanged, run, asyncResult => {
-      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.log(asyncResult.error.message);
-        return;
-      }
-
-      console.log("Event handler added.");
-    });
-
-    const httpString2 = document.getElementById("joinedArrayDiv");
-    httpString2.textContent = "So ein wunderschöner Tag";
-
     run();
     
   }
@@ -31,7 +18,7 @@ Office.onReady((info) => {
 
 async function run() {
   // Get a reference to the current message
-  //*const item = Office.context.mailbox.item;
+  const item = Office.context.mailbox.item;
   // Write message property value to the task pane
   // document.getElementById("item-subject").innerHTML = "<b>Betreff:</b> <br/>" + item.subject;
   // document.getElementById("item-attachments").innerHTML = "<b>Anzahl Anlagen:</b> <br/>" + item.attachments.length;
@@ -43,38 +30,12 @@ async function run() {
   //const from = item.from.emailAddress;
   //const toRecipient = item.to[0]["emailAddress"];
 
-  //*const mailGUID = encodeURIComponent(item.itemId);
-  //*const subject = encodeURIComponent(item.subject);
+  const mailGUID = encodeURIComponent(item.itemId);
+  const subject = encodeURIComponent(item.subject);
   const date = encodeURIComponent(item.dateTimeCreated.toISOString());
 
   //console.log("Halli hallooo");
   console.log("Datum = " + date);
 
-  const httpString = document.getElementById("joinedArrayDiv");
-  httpString.textContent = "Hello again - Howard Carpendale";
-
-  Office.context.mailbox.getSelectedItemsAsync(asyncResult => {
-    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.log(asyncResult.error.message);
-        return;      
-    }
-
-    asyncResult.value.forEach((item, index) => {
-        let mailGuidArray = [];
-        let mailGuidKey = "mailGUID" + (parseInt(++index));
-        let mailGuidValue = encodeURIComponent(item.itemId);
-        mailGuidArray.push({[mailGuidKey]: mailGuidValue});
-        let joinedArray = "";
-
-        for (const arrayItem of mailGuidArray) {
-          let keys = Object.keys(arrayItem);
-          for (const key of keys) {
-            joinedArray += "&" + key + "=" + mailGuidArray[key];
-          }
-        }
-    });
-  });
-
-
-  //*document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject + "&mailDate=" + date);
+  document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject + "&mailDate=" + date);
 }
