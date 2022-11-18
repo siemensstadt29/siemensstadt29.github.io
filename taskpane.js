cf9c7c843264f187ae4f1c347833ba6b65682dbe
siemensstadt29 <stefan_gierke@web.de>
2022-11-18 20:29:09 +0100

    Files uploaded

--- taskpane.js
+++ taskpane.js
@@ -11,14 +11,14 @@ Office.onReady((info) => {
     console.log("Das Skript wurde erfolgreich geladen!");
 
     // Register an event handler to identify when messages are selected.
-    Office.context.mailbox.addHandlerAsync(Office.EventType.SelectedItemsChanged, run, asyncResult => {
-      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.log(asyncResult.error.message);
-        return;
-      }
+    // Office.context.mailbox.addHandlerAsync(Office.EventType.SelectedItemsChanged, run, asyncResult => {
+    //   if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+    //     console.log(asyncResult.error.message);
+    //     return;
+    //   }
 
-      console.log("Event handler added.");
-    });
+    //   console.log("Event handler added.");
+    // });
 
     run();
     
@@ -40,40 +40,40 @@ async function run() {
   //const from = item.from.emailAddress;
   //const toRecipient = item.to[0]["emailAddress"];
 
-  // *const mailGUID = encodeURIComponent(item.itemId);
-  // *const subject = encodeURIComponent(item.subject);
-  // *const date = encodeURIComponent(item.dateTimeCreated.toISOString());
+  const mailGUID = encodeURIComponent(item.itemId);
+  const subject = encodeURIComponent(item.subject);
+  const date = encodeURIComponent(item.dateTimeCreated.toISOString());
 
-  // *console.log("Halli hallooo");
-  // *console.log("Datum = " + date);
+  //console.log("Halli hallooo");
+  console.log("Datum = " + date);
 
-  Office.context.mailbox.getSelectedItemsAsync(asyncResult => {
-    if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-        console.log(asyncResult.error.message);
-        return;      
-    }
+//   Office.context.mailbox.getSelectedItemsAsync(asyncResult => {
+//     if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+//         console.log(asyncResult.error.message);
+//         return;      
+//     }
 
-    asyncResult.value.forEach((item, index) => {
-        let mailGuidArray = [];
-        let mailGuidKey = "mailGUID" + (parseInt(++index));
-        let mailGuidValue = encodeURIComponent(item.itemId);
-        mailGuidArray.push({[mailGuidKey]: mailGuidValue});
-        let joinedArray = "";
+//     asyncResult.value.forEach((item, index) => {
+//         let mailGuidArray = [];
+//         let mailGuidKey = "mailGUID" + (parseInt(++index));
+//         let mailGuidValue = encodeURIComponent(item.itemId);
+//         mailGuidArray.push({[mailGuidKey]: mailGuidValue});
+//         let joinedArray = "";
 
-        for (const arrayItem of mailGuidArray) {
-          let keys = Object.keys(arrayItem);
-          for (const key of keys) {
-            joinedArray += "&" + key + "=" + mailGuidArray[key];
-          }
-        }
+//         for (const arrayItem of mailGuidArray) {
+//           let keys = Object.keys(arrayItem);
+//           for (const key of keys) {
+//             joinedArray += "&" + key + "=" + mailGuidArray[key];
+//           }
+//         }
 
-        const httpString = document.getElementById("joinedArrayDiv");
-        httpString.innerText = joinedArray;
-    });
-});
+//         const httpString = document.getElementById("joinedArrayDiv");
+//         httpString.innerText = joinedArray;
+//     });
+// });
 
   //const httpString = document.getElementById("joinedArrayDiv");
   //httpString.innerText = joinedArray;
 
-  //document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject + "&mailDate=" + date);
+  document.getElementById("powerapps-iframe").setAttribute("src", "https://apps.powerapps.com/play/9c9d578c-eb09-4c0c-81a4-9df8ecca7650?tenantId=b9d0cec0-1f3e-4a91-8ec5-7341d8dbe1e0&source=iframe&mailGuid=" + mailGUID + "&subject=" + subject + "&mailDate=" + date);
 }
